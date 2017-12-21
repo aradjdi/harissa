46715cead3c019237339f2285ef368ba0137ebc4
dvallartamendes <diego.vallartamendes@mousquetaires.com>
2017-12-21 12:20:54 +0100

    feat(timer): time run-smartphone-android action, other actions to come

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,58 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const timer = require('../scripts/_timer');
+
+const time = (scriptPath) => {
+    timer.start();
+
+    require(scriptPath)
+        .init()
+        .then(() => console.log('cordova finish'))
+        .then(() => timer.end())
+        .then(duration => console.log(`BUILD SUCCESSFUL in ${duration}s`));
+};
+
+program.version(require('../package').version);
 
 program
-  .command('init')
-  .action(() => require('../scripts/init'));
+    .command('init')
+    .action(() => require('../scripts/init'));
 
 program
-  .command('release')
-  .action(() => require('../scripts/release'));
+    .command('release')
+    .action(() => require('../scripts/release'));
 
 program
-  .command('release-smartphone')
-  .action(() => require('../scripts/release-smartphone'));
+    .command('release-smartphone')
+    .action(() => require('../scripts/release-smartphone'));
 
 program
-  .command('release-tablet')
-  .action(() => require('../scripts/release-tablet'));
+    .command('release-tablet')
+    .action(() => require('../scripts/release-tablet'));
 
 program
-  .command('run-smartphone-android')
-  .action(() => require('../scripts/run-smartphone-android'));
+    .command('run-smartphone-android')
+    .action(() => time('../scripts/run-smartphone-android'));
 
 program
-  .command('run-smartphone-ios')
-  .action(() => require('../scripts/run-smartphone-ios'));
+    .command('run-smartphone-ios')
+    .action(() => require('../scripts/run-smartphone-ios'));
 
 program
-  .command('run-tablet-android')
-  .action(() => require('../scripts/run-tablet-android'));
+    .command('run-tablet-android')
+    .action(() => require('../scripts/run-tablet-android'));
 
 program
-  .command('run-tablet-ios')
-  .action(() => require('../scripts/run-tablet-ios'));
+    .command('run-tablet-ios')
+    .action(() => require('../scripts/run-tablet-ios'));
 
 program
-  .command('serve')
-  .action(() => require('../scripts/serve'));
+    .command('serve')
+    .action(() => require('../scripts/serve'));
 
 program
-  .command('serve-vue')
-  .action(() => require('../scripts/serve-vue'));
+    .command('serve-vue')
+    .action(() => require('../scripts/serve-vue'));
 
 program.parse(process.argv);
