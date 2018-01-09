5cd9cdac3a2fa2ffa818722b0c4ae6c2778dad23
dvallartamendes <diego.vallartamendes@mousquetaires.com>
2018-01-09 18:26:37 +0100

    feat(appaloosa-client): use appaloosa-client for node.js to upload packages

--- scripts/_deploy-exec.js
+++ scripts/_deploy-exec.js
@@ -1,12 +1,16 @@
-const exec = require('./_exec');
-const paths = require('./_paths');
+const Q = require('q');
+require('./_spy');
 
-const storeToken = 'g58kzdxrvdtlhnumiv3wd93z95hq6khm';
+const appaloosa = require('appaloosa-client');
 
-const uploadPackage = filepath => 
-    exec.executeCommand(`ruby ${paths.rootDir}/appaloosa-client.rb ${storeToken} ${filepath}`)
-        .catch(() => uploadPackage(filepath));
+const uploadPackage = (filepath, storeToken, groups, changes) => Q().spy(() => appaloosa.upload(storeToken, filepath, groups, changes), 'appaloosa', 'upload');
 
 module.exports = {
-    uploadPackage
+    uploadPackage(filepath, storeToken, groups, changes) {
+        if (!filepath || !storeToken || !groups || !changes) {
+            throw new Error('All arguments are mandatory');
+        }
+
+        uploadPackage(filepath, storeToken, groups, changes);
+    }
 };
