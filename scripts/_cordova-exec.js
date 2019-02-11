ae80db76305cb1d1d20a89f54aaf59bdf283583a
Ahmed Radjdi <ahmed.radjdi-ext@mousquetaires.com>
2019-02-11 17:38:10 +0100

    chore(): use local cordova package

--- scripts/_cordova-exec.js
+++ scripts/_cordova-exec.js
@@ -3,15 +3,15 @@ const Q = require('q');
 const exec = require('./_exec');
 
 const createProject = (appPath, id, name, templatePath) => Q()
-    .then(() => exec.executeCommand(`cordova create ${appPath} ${id} ${name} --template ${templatePath}`))
-    .then(() => exec.executeCommand('cordova prepare', appPath));
+    .then(() => exec.executeCommand(`npx cordova create ${appPath} ${id} ${name} --template ${templatePath}`))
+    .then(() => exec.executeCommand('npx cordova prepare', appPath));
 
 const packageProject = projectDir => Q()
-    .then(() => exec.executeCommand('cordova build ios --device --release', projectDir))
-    .then(() => exec.executeCommand('cordova build android --release', projectDir));
+    .then(() => exec.executeCommand('npx cordova build ios --device --release', projectDir))
+    .then(() => exec.executeCommand('npx cordova build android --release', projectDir));
 
 const runProject = (platform, projectDir) => Q()
-    .then(() => exec.executeCommand(`cordova run ${platform}`, projectDir));
+    .then(() => exec.executeCommand(`npx cordova run ${platform}`, projectDir));
 
 module.exports = {
     createProject,
