d6ad10af7ea656c0a6720fa9912304a3e77c9eaa
Ahmed Radjdi <ahmed.radjdi-ext@mousquetaires.com>
2019-08-13 12:08:31 +0200

    fix(appaloosa): 🔒 - use token from conf file

--- scripts/_deploy.js
+++ scripts/_deploy.js
@@ -3,16 +3,10 @@ const Q = require('q');
 const paths = require('./_paths');
 const deployExec = require('./_deploy-exec');
 
-const APPALOOSA_TOKENS = {
-    dev: 'g58kzdxrvdtlhnumiv3wd93z95hq6khm',
-    preprod: 'g58kzdxrvdtlhnumiv3wd93z95hq6khm',
-    prod: 'ihdmighsgdvftx29xpiclqikaufstx5f'
-};
-
-const getAppaloosaToken = () => APPALOOSA_TOKENS[process.env.NODE_ENV];
 const getCordovaDir = () => `${paths.cordovaDir}/${process.env.NODE_ENV}`;
 
 const getAppaloosaGroups = () => require(`${paths.confDir}/${process.env.NODE_ENV}/appaloosa.conf.json`).APPALOOSA_GROUPS;
+const getAppaloosaToken = () => require(`${paths.confDir}/${process.env.NODE_ENV}/appaloosa.conf.json`).APPALOOSA_STORE_TOKEN;
 
 const uploadPackages = (device, changes) => {
     const appName = require(`${getCordovaDir()}/${device}/package.json`).displayName;
